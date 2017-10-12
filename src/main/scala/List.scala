5b6e84bbf5a16b7708c81c1bbbe327c7b93c7e4d
Arnold Farkas <farkasarnold1981@gmail.com>
2017-10-12 15:15:27 +0200

    done together

--- src/main/scala/List.scala
+++ src/main/scala/List.scala
@@ -49,41 +49,71 @@ object List {
   def product2(ns: List[Double]) =
     foldRight(ns, 1.0)(_ * _) // `_ * _` is more concise notation for `(x,y) => x * y`; see sidebar
 
-  def tail[A](l: List[A]): List[A] = ???
+  def tail[A](l: List[A]): List[A] = l match {
+    case Cons(_, as) => as
+    case _ => Nil
+  }
 
-  def setHead[A](l: List[A], h: A): List[A] = ???
+  def setHead[A](l: List[A], h: A): List[A] = l match {
+    case Cons(_, as) => Cons(h, as)
+    case _ => Nil
+  }
 
-  def drop[A](l: List[A], n: Int): List[A] = ???
+  def drop[A](l: List[A], n: Int): List[A] = l match {
+    case Cons(_, as) if n > 0 => drop(as, n - 1)
+    case _ => l
+  }
 
-  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = ???
+  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = l match {
+    case Cons(a, as) if f(a) => dropWhile(as, f)
+    case _ => l
+  }
 
   //  Returns the list without its last element.
-  def init[A](l: List[A]): List[A] = ???
+  def init[A](l: List[A]): List[A] = l match {
+    case Cons(a, as @ Cons(_, _)) => Cons(a, init(as))
+    case _ => Nil
+  }
 
-  def length[A](l: List[A]): Int = ???
+  def length[A](l: List[A]): Int = l match {
+    case Cons(_, as) => 1 + length(as)
+    case _ => 0
+  }
 
-  def foldLeft[A, B](l: List[A], z: B)(f: (B, A) => B): B = ???
+  @tailrec
+  def foldLeft[A, B](l: List[A], z: B)(f: (B, A) => B): B = l match {
+    case Nil => z
+    case Cons(a, as) => foldLeft(as, f(z, a))(f)
+  }
 
-  def sum2FL(ns: List[Int]) = ???
+  def sum2FL(ns: List[Int]) = foldLeft(ns, 0)((x, y) => {
+    x + y
+  })
 
-  def product2FL(ns: List[Double]) = ???
+  def product2FL(ns: List[Double]) = foldLeft[Double, Double](ns, 1)(_*_)
 
-  def lengthFL[A](l: List[A]): Int = ???
+  def lengthFL[A](l: List[A]): Int = foldLeft(l, 0)((b, _) => b + 1)
 
-  def tailFL[A](l: List[A]): List[A] = ???
+//  def tailFL[A](l: List[A]): List[A] = foldLeft[A, List[A]](l, Nil)((b, a) => Cons(a, b))
 
 //  @tailrec
 //  def append[A](a1: List[A], a2: List[A]): List[A] = ???
 
-  def reverse[A](l: List[A]): List[A] = ???
+  def reverse[A](l: List[A]): List[A] = foldLeft[A, List[A]](l, Nil)((b, a) => Cons(a, b))
 
-  def foldRightViaFoldLeft[A, B](as: List[A], z: B)(f: (A, B) => B): B = ???
+  def foldRightViaFoldLeft[A, B](as: List[A], z: B)(f: (A, B) => B): B = foldLeft(reverse(as), z){
+    case (b, a) => f(a, b)
+  }
 
-  def map[A, B](l: List[A])(f: A => B): List[B] = ???
+  def map[A, B](l: List[A])(f: A => B): List[B] = foldRightViaFoldLeft[A, List[B]](l, Nil){
+    case (a, bs) => Cons(f(a), bs)
+  }
 
-  def flatten[A](l: List[List[A]]): List[A] = ???
+  def flatten[A](l: List[List[A]]): List[A] = foldLeft[List[A], List[A]](l, Nil){
+    case (aggr, as) => append(aggr, as)
+  }
 
-  def flatMap[A, B](l: List[A])(f: A => List[B]): List[B] = ???
+  def flatMap[A, B](l: List[A])(f: A => List[B]): List[B] = flatten(map(l)(f))
 
   def fromSeq[A](seq: Seq[A]): List[A] = ???
 
@@ -93,23 +123,30 @@ object List {
 object TestList extends App {
 
   import List._
-//
-//  assert(init(Nil) ==
-//    Nil
-//  )
-//
-//  assert(init(List(1)) ==
-//    Nil
-//  )
-//
-//  assert(init(List(1, 2)) ==
-//    List(1)
-//  )
-//
-//  assert(init(List(1, 2, 3)) ==
-//    List(1, 2)
-//  )
-//
+
+  assert(init(Nil) ==
+    Nil
+  )
+
+  assert(init(List(1)) ==
+    Nil
+  )
+
+  assert(init(List(1, 2)) ==
+    List(1)
+  )
+
+  assert(init(List(1, 2, 3)) ==
+    List(1, 2)
+  )
+
+  println(map(List(1, 2, 3))(a => a *a ))
+
+
+  println(flatten(List(List(1, 2, 3), List(4, 5, 6))))
+
+
+  //
 //  assert(reverse(List(1, 2, 3)) ==
 //    List(3, 2, 1)
 //  )
@@ -133,16 +170,16 @@ object TestList extends App {
 //  assert(flatten(List(List(1, 2, 3), List(4, 5), List(), List(6))) ==
 //    List(1, 2, 3, 4, 5, 6)
 //  )
-//
-//  def wordLengths(i: Int): List[String] = {
-//
-//    def go(n: Int): List[String] =
-//      if (n > 0) Cons(n.toString, go(n - 1))
-//      else List()
-//
-//    reverse(go(i))
-//  }
-//
+
+  def wordLengths(i: Int): List[String] = {
+
+    def go(n: Int): List[String] =
+      if (n > 0) Cons(n.toString, go(n - 1))
+      else List()
+
+    reverse(go(i))
+  }
+
 //  assert(wordLengths(2) ==
 //    List("1", "2")
 //  )
@@ -156,39 +193,39 @@ object TestList extends App {
 //  )
 //
 //  assert(length(wordLengths(50)) == 50)
-//
-//  assert(flatMap(List(2))(wordLengths) ==
-//    List("1", "2")
-//  )
-//  assert(flatMap(List())(wordLengths) ==
-//    List()
-//  )
-//
-//  assert(flatMap(List(0))(wordLengths) ==
-//    List()
-//  )
-//
-//  assert(flatMap(List(0, 0))(wordLengths) ==
-//    List()
-//  )
-//
-//  assert(flatMap(List(0, 1))(wordLengths) ==
-//    List("1")
-//  )
-//
-//  assert(flatMap(List(0, 2))(wordLengths) ==
-//    List("1", "2")
-//  )
-//
-//  assert(flatMap(List(0, 2, 1))(wordLengths) ==
-//    List("1", "2", "1")
-//  )
-//
-//  assert(flatMap(List(0, 2, 1, 3))(wordLengths) ==
-//    List("1", "2", "1", "1", "2", "3")
-//  )
-//
-//  assert(flatMap(List(0, 2, 1, 3, 0, 1))(wordLengths) ==
-//    List("1", "2", "1", "1", "2", "3", "1")
-//  )
+
+  assert(flatMap(List(2))(wordLengths) ==
+    List("1", "2")
+  )
+  assert(flatMap(List())(wordLengths) ==
+    List()
+  )
+
+  assert(flatMap(List(0))(wordLengths) ==
+    List()
+  )
+
+  assert(flatMap(List(0, 0))(wordLengths) ==
+    List()
+  )
+
+  assert(flatMap(List(0, 1))(wordLengths) ==
+    List("1")
+  )
+
+  assert(flatMap(List(0, 2))(wordLengths) ==
+    List("1", "2")
+  )
+
+  assert(flatMap(List(0, 2, 1))(wordLengths) ==
+    List("1", "2", "1")
+  )
+
+  assert(flatMap(List(0, 2, 1, 3))(wordLengths) ==
+    List("1", "2", "1", "1", "2", "3")
+  )
+
+  assert(flatMap(List(0, 2, 1, 3, 0, 1))(wordLengths) ==
+    List("1", "2", "1", "1", "2", "3", "1")
+  )
 }
