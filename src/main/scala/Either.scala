8a59ae2ba8f2a4f73900335fa459813f5b91081b
Arnold Farkas <farkasarnold1981@gmail.com>
2017-11-15 16:42:04 +0100

    either

--- src/main/scala/Either.scala
+++ src/main/scala/Either.scala
@@ -3,21 +3,76 @@ package fpinscala.errorhandling
 import scala.{Option => _, Either => _, Left => _, Right => _, _} // hide std library `Option` and `Either`, since we are writing our own in this chapter
 
 sealed trait Either[+E,+A] {
-  def map[B](f: A => B): Either[E, B] = ???
 
-  def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = ???
+  def map[B](f: A => B): Either[E, B] = this match {
+    case Right(a) => Right(f(a))
+    case Left(a) => Left(a)
+  }
 
-  def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = ???
+  def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = this match {
+    case Right(a) => f(a)
+    case Left(e) => Left(e)
+  }
 
-  def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = ???
-}
+  def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = this match {
+    case Right(_) => this
+    case Left(_) => b
+  }
+
+
+//  def map2[A, B, C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = for {
+//    aa <- a
+//    bb <- b
+//  } yield f(aa, bb)
+
+  def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] =
+//    this.flatMap{aa =>
+//      b.map(bb => f(aa, bb))
+//    }
+    for {
+      aa <- this
+      bb <- b
+    } yield f(aa, bb)
+
+  }
 case class Left[+E](get: E) extends Either[E,Nothing]
 case class Right[+A](get: A) extends Either[Nothing,A]
 
 object Either {
-  def traverse[E,A,B](es: List[A])(f: A => Either[E, B]): Either[E, List[B]] = ???
 
-  def sequence[E,A](es: List[Either[E,A]]): Either[E,List[A]] = ???
+  def traverse[E,A,B](es: List[A])(f: A => Either[E, B]): Either[E, List[B]] =
+    List.foldRightViaFoldLeft[A, Either[E, List[B]]](es, Right(Nil))(f(_).map2(_)(Cons(_, _)))
+
+//    es match {
+//    case Nil => Right(Nil)
+//    case Cons(a, as) =>
+//      val either = f(a)
+//      val next = traverse(as)(f)
+//
+//      either.map2(next)(Cons(_, _))
+//
+//      for {
+//        aa <- either
+//        list <- next
+//      } yield Cons(aa, list)
+//  }
+
+
+//  def sequence[A](oas: List[Option[A]]): Option[List[A]] = List
+//    .foldRightViaFoldLeft[Option[A], Option[List[A]]](oas, Some(List())) {
+//    case (elem, elements) => for {
+//      as <- elements
+//      a <- elem
+//    } yield Cons(a, as)
+//  }
+
+  def sequence[E,A](es: List[Either[E,A]]): Either[E,List[A]] =
+    List.foldRightViaFoldLeft[Either[E,A],Either[E, List[A]]](es, Right(Nil)){
+        for {
+          ax <- _
+          aax <- _
+        } yield Cons(ax, aax)
+    }
 
   def mean(xs: IndexedSeq[Double]): Either[String, Double] =
     if (xs.isEmpty)
@@ -32,5 +87,33 @@ object Either {
   def Try[A](a: => A): Either[Exception, A] =
     try Right(a)
     catch { case e: Exception => Left(e) }
+
+}
+
+object Exaxmple extends  App {
+
+  def sqrt(d: Double): Either[String, Double] =
+    if (d >= 0) Right(Math.sqrt(d))
+    else Left(s"$d is less than 0")
+
+  println(Right(1).map(_ * 3))
+
+  val left : Either[Int, Int] = Left(1)
+
+  println(left.map(_ * 3))
+
+  val rightPos: Either[String, Double] = Right(4)
+  val leftStr: Either[String, Double] = Left(":(")
+  val rightNeg: Either[String, Double] = Right(-1)
+
+  println(rightPos.flatMap(sqrt))
+  println(
+    for {
+      d      <- rightPos
+      result <- sqrt(d)
+    } yield result
+  )
+  println(leftStr.flatMap(sqrt))
+  println(rightNeg.flatMap(sqrt))
 
 }
--- src/test/scala/EitherSpec.scala
+++ src/test/scala/EitherSpec.scala
@@ -1,16 +1,75 @@
 import org.scalacheck.Prop.forAll
+import org.scalacheck.{Gen, Properties}
 import org.scalatest.FlatSpec
 import org.scalatest.prop.Checkers
+import scala.{Either => _, Left => _, Option => _, Right => _, _}
+import fpinscala.errorhandling.Either
+import fpinscala.errorhandling.Left
+import fpinscala.errorhandling.Right
+import fpinscala.errorhandling.Either._
+import fpinscala.errorhandling.List
+import fpinscala.errorhandling.List._
+import org.scalacheck.Arbitrary.arbitrary
+import org.scalacheck.{Arbitrary, Gen, Properties}
+import org.scalacheck.Gen.{Parameters, alphaLowerChar, alphaUpperChar, listOf}
+object EitherSpec extends Properties("Either") {
 
-class EitherSpec extends FlatSpec with Checkers {
+  def rightGen: Gen[Right[Int]] = for {
+    i <- arbitrary[Int]
+  } yield Right(i)
 
-  behavior of "either"
+  def leftGen: Gen[Left[String]] = for {
+    str <- Gen.alphaLowerStr
+  } yield Left(str)
 
-  it should "???" in {
-    check {
-      forAll { e: Either[String, Int] =>
-        ???
-      }
-    }
+  def eitherGen: Gen[Either[String, Int]] = for {
+    bool <- arbitrary[Boolean]
+    l <- leftGen
+    r <- rightGen
+  } yield if(bool) r else l
+
+  implicit lazy val arbLeft: Arbitrary[Left[String]] = Arbitrary(leftGen)
+  implicit lazy val arbRight: Arbitrary[Right[Int]] = Arbitrary(rightGen)
+  implicit lazy val arbEither: Arbitrary[Either[String, Int]] = Arbitrary(eitherGen)
+
+  property("map left") = forAll{l: Left[String] =>
+    l.map(_.toString) == l
   }
+
+  property("map right") = forAll{r: Right[Int] =>
+    r.map(_ - 1) == Right(r.get - 1)
+  }
+
+  property("map either") = forAll{ei: Either[String, Int] =>
+
+    val there = ei.map(_ - 1)
+//    val there = ei.map(_ * 3)
+
+    val forth = there.map(_ + 1)
+//    val forth = there.map(_ / 3)
+
+//    println(ei)
+//    println(there)
+//    println(forth)
+//    println()
+
+    ei == forth
+  }
+
+  property("map2 either") = forAll{(e1: Either[String, Int], e2: Either[String, Int]) =>
+
+    def sumStr(i1: Int, i2: Int) = s"sum=${i1 + i2}"
+
+    val mapped = e1.map2(e2)(sumStr)
+
+    val expected = (e1, e2) match {
+      case (Right(r1), Right(r2)) => Right(sumStr(r1, r2))
+      case (Right(_), Left(l)) => Left(l)
+      case (Left(l), _) => Left(l)
+    }
+
+    expected == mapped
+  }
+
+
 }
